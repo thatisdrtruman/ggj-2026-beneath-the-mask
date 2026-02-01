7b90933c349da74a3acc17356d6f714c11a4dddb
duranmi <79401404+duranmi@users.noreply.github.com>
2026-02-01 00:52:32 +0000

    Added sounds to main menu and first dialogue
    Added sounds to main menu and first dialogue

--- scripts/first_dialogue.gd
+++ scripts/first_dialogue.gd
@@ -1,8 +1,19 @@
 extends Control
 
+var audio_manager: AudioManager = null
 var res = load("res://assets/dialogues/dialogue_one.dialogue")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	audio_manager = AudioManager.new()
+	var dialogue_theme: AudioManagerPlus = AudioManagerPlus.new()
+	dialogue_theme.stream = preload("res://assets/sounds/msc_dialogue.ogg")
+	dialogue_theme.volume_db = -6.0
+	dialogue_theme.loop = true
+	
+	add_child(audio_manager)
+	audio_manager.add_plus("dialogue", dialogue_theme)
+	audio_manager.play_plus("dialogue")
 	
 	DialogueManager.show_dialogue_balloon(res, "start")
 	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
@@ -15,4 +26,5 @@ func _process(delta):
 
 func _on_dialogue_ended(res):
 	print("dialogue ended")
+	# change to first level scene
 	
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,30 @@
 extends Control
 
+var audio_manager: AudioManager = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	audio_manager = AudioManager.new()
+	var main_menu_theme: AudioManagerPlus = AudioManagerPlus.new()
+	main_menu_theme.stream = preload("res://assets/sounds/msc_main_menu.ogg")
+	main_menu_theme.volume_db = -10.0
+	main_menu_theme.loop = true
+	
+	var button_focus_sfx: AudioManagerPlus = AudioManagerPlus.new()
+	button_focus_sfx.stream = preload("res://assets/sounds/sfx_Menu_Navigation_01.wav")
+	button_focus_sfx.use_clipper = true
+	button_focus_sfx.start_time = 0.0
+	button_focus_sfx.end_time = 0.5
+	
+	var game_start_sfx: AudioManagerPlus = AudioManagerPlus.new()
+	game_start_sfx.stream = preload("res://assets/sounds/sfx_Start_Game_Selection.wav")
+
+	add_child(audio_manager)
+	audio_manager.add_plus("main_menu", main_menu_theme)
+	audio_manager.add_plus("button_focus", button_focus_sfx)
+	audio_manager.add_plus("game_start", game_start_sfx)
+	
+	audio_manager.play_plus("main_menu")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,5 +33,18 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
+	audio_manager.play_plus("game_start")
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://scenes/first_dialogue.tscn")
-	pass # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+
+func _on_start_button_mouse_entered():
+	audio_manager.play_plus("button_focus")
+
+
+func _on_exit_button_mouse_entered():
+	audio_manager.play_plus("button_focus")
