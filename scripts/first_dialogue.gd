43423ab203ed3e9c4aeb3325036477ca52c3800b
duranmi <79401404+duranmi@users.noreply.github.com>
2026-02-01 13:36:42 +0000

    Implemented dialogue typing sfx

--- scripts/first_dialogue.gd
+++ scripts/first_dialogue.gd
@@ -11,11 +11,16 @@ func _ready():
 	dialogue_theme.volume_db = -6.0
 	dialogue_theme.loop = true
 	
+	var dialogue_typing_sfx: AudioManagerPlus = AudioManagerPlus.new()
+	dialogue_typing_sfx.stream = preload("res://assets/sounds/sfx_Dialogue_Typing.wav")
+	
 	add_child(audio_manager)
 	audio_manager.add_plus("dialogue", dialogue_theme)
+	audio_manager.add_plus("dialogue_typing", dialogue_typing_sfx)
 	audio_manager.play_plus("dialogue")
 	
 	DialogueManager.show_dialogue_balloon(res, "start")
+	DialogueManager.got_dialogue.connect(_on_got_dialogue)
 	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 	pass
 
@@ -23,6 +28,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_got_dialogue(line):
+	audio_manager.play_plus("dialogue_typing")
+
 
 func _on_dialogue_ended(res):
 	get_tree().change_scene_to_file("res://main.tscn")
